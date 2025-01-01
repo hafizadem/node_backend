0581a119bcf00d345a9122ebccc224b72ba2a79f
hafiz adem <hafizadem71@gmail.com>
2025-01-01 06:25:46 -0800

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
-const lofi = require('./fs_sync.js')
-const http = require('http')
+const {readFile, writeFile} =require('fs').promises
+const util = require('util')
+
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
 
-const server = http.createServer((req,res)=>{
-    if (req.url === '/home') {
-        res.end('this is our home page')
-    
-    
-    if (req.url === '/') 
-        res.end('nothing is hear')
-        
-  
- 
- }
- else{
-    res.end('nothing')
- }
-   
-})
-console.log('jjj')
-server.listen(9000)
+
+// const getTheFile = (path)=>{
+//     return new Promise((resolve, reject)=>{
+//         readFile(path,'utf-8',(err, data)=>{
+//             if(err){
+//                 reject(err)
+//             }
+//             resolve(data)
+//         })
+//     })
+// }
+
+const start = async()=>{
+    try{
+        const first = await readFile('./src/first.txt','utf-8')
+        const second = await readFile('./src/second.txt','utf-8')
+        const thired = await readFile('./src/sorce/more.txt','utf-8')
+        await writeFile('./src/sorce/more.txt',`this message is from app.js: ${  first } ${second}`)
+        console.log(first,second, thired)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start()
+
+
+
+
+// getTheFile('./src/first.txt')
+// .then((data)=>{
+//     console.log(data)
+// }).catch((err)=>{
+//     console.log(err)
+// })
 
--- fs_sync.js
+++ fs_sync.js
@@ -1,17 +1,11 @@
-
-
-
-
+// filepath: /c:/Users/boos/Documents/js hindi/fs_sync.js
 const { readFileSync, writeFileSync } = require('fs')
 
 const first = readFileSync('./src/first.txt','utf8')
 const second = readFileSync('./src/second.txt','utf8')
 
- writeFileSync('./src/sorce/more.txt',`this message is from app.js${  first}`)
+writeFileSync('./src/sorce/more.txt',`this message is from app.js${  first}`)
 
-module.exports={first,second}
+
 
 console.log(first,second)
-
-
-
